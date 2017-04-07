ba969d2710dff483863cd3d956745ac3ef738b0d
SoraYuki <sorayuki@live.com>
2017-04-07 18:25:53 +0800

    update version. solve lib conflict between debug and release configuration. use static for release x64 build

--- src/vsfilter/resource.h
+++ src/vsfilter/resource.h
@@ -1,7 +1,11 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by VSFilter.rc
+// Microsoft Visual C++ 生成的包含文件。
+// 供 VSFilterMod.rc 使用
 //
+#define VERSION_PATCH                   0
+#define VERSION_MAJOR                   1
+#define VERSION_MINOR                   1
+#define VERSION_REV                     90
 #define IDS_PROJNAME                    100
 #define IDD_DVSPATHSPAGE                101
 #define IDS_PROJNAME2                   102
@@ -179,7 +183,7 @@
 #define IDC_COLORSHAD                   1066
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        231
